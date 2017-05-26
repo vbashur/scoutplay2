2be16554043502e3ec8b53b506f561149ee600ed
Bashurov <victor.bashurov@wirecard.lan>
2017-05-26 18:11:11 +0200

    Added Ads controller and basic routes

--- app/controllers/AdController.scala
+++ app/controllers/AdController.scala
@@ -1,8 +1,19 @@
 package controllers
 
-/**
-  * Created by victor.bashurov on 26.05.2017.
-  */
-class AdController {
+import play.api.mvc.Controller
+
+
+class AdController extends Controller {
+
+  def index = TODO
+
+  def get(id: String) = TODO;
+
+  def add = TODO;
+
+  def update(id: String) = TODO;
+
+  def delete(id: String) = TODO;
+
 
 }
