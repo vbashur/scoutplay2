d960e8e57c0c6bba48968a5fac41503179563bbc
Bashurov <victor.bashurov@wirecard.lan>
2017-05-27 01:31:00 +0200

    Updated model representation and controller implementation

--- app/controllers/AdController.scala
+++ app/controllers/AdController.scala
@@ -2,10 +2,13 @@ package controllers
 
 import javax.inject.Inject
 
+import model.{Advertisement, FuelType}
 import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, BodyParsers, Controller}
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.bson.{BSONDocument, BSONLong, BSONObjectID, BSONString}
 import repositories.AdRepositoryImpl
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class AdController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends Controller
@@ -17,13 +20,47 @@ class AdController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends Con
     adRepository.find().map(adv => Ok(Json.toJson(adv)))
   }
 
-  def get(id: String) = TODO;
+  def get(id: String) = Action.async { implicit request =>
+    adRepository.get(BSONDocument(Advertisement.field_Id -> BSONString(id))).map(adv => Ok(Json.toJson(adv)))
+  }
 
-  def add = TODO;
+  def add = Action.async(BodyParsers.parse.json) { implicit request =>
+    val title = (request.body \ Advertisement.field_Title).as[String]
+    val fuelType = (request.body \  Advertisement.field_Fuel).as[String]
+    val mileage = (request.body \  Advertisement.field_Mile).as[Long]
+    val isNew = (request.body \  Advertisement.field_New).as[Boolean]
+    val price = (request.body \  Advertisement.field_Price).as[Long]
+    val registration = (request.body \  Advertisement.field_Reg).as[String]
+    adRepository.save(BSONDocument(
+      Advertisement.field_Title -> title,
+      Advertisement.field_Fuel -> fuelType,
+      Advertisement.field_Mile -> mileage,
+      Advertisement.field_New -> isNew,
+      Advertisement.field_Price -> price,
+      Advertisement.field_Reg -> registration
+    )).map(result => Created)
+  }
 
-  def update(id: String) = TODO;
+  def update(id: String) = Action.async(BodyParsers.parse.json) { implicit request =>
+    val title = (request.body \ Advertisement.field_Title).as[String]
+    val fuelType = (request.body \  Advertisement.field_Fuel).as[String]
+    val mileage = (request.body \  Advertisement.field_Mile).as[Long]
+    val isNew = (request.body \  Advertisement.field_New).as[Boolean]
+    val price = (request.body \  Advertisement.field_Price).as[Long]
+    val registration = (request.body \  Advertisement.field_Reg).as[String]
+    adRepository.update(BSONDocument(Advertisement.field_Id -> BSONString(id)),
+      BSONDocument("$set" -> BSONDocument(
+        Advertisement.field_Title -> title,
+        Advertisement.field_Fuel -> fuelType,
+        Advertisement.field_Mile -> mileage,
+        Advertisement.field_New -> isNew,
+        Advertisement.field_Price -> price,
+        Advertisement.field_Reg -> registration
+      )))
+      .map(result => Accepted)
+  }
 
-  def delete(id: String) = TODO;
-
+  def delete(id: String) = adRepository.remove(BSONDocument(Advertisement.field_Id -> BSONString(id)))
+    .map(result => Accepted);
 
 }
--- app/model/Advertisement.scala
+++ app/model/Advertisement.scala
@@ -11,13 +11,13 @@ abstract class Advertisement(id: Long,
                              mileage: Long,
                              firstRegistration: Option[DateTime]) {
   def toJsonObj = JsObject(Seq(
-    "id" -> JsNumber(id),
-    "title" -> JsString(title),
-    "fuel" -> JsString(fuel.toString),
-    "price" -> JsNumber(price),
-    "new" -> JsBoolean(isNew),
-    "mileage" -> JsNumber(mileage),
-    "firstRegistration" -> JsString(firstRegistration  match {
+    Advertisement.field_Id -> JsNumber(id),
+    Advertisement.field_Title -> JsString(title),
+    Advertisement.field_Fuel -> JsString(fuel.toString),
+    Advertisement.field_Price -> JsNumber(price),
+    Advertisement.field_New -> JsBoolean(isNew),
+    Advertisement.field_Mile -> JsNumber(mileage),
+    Advertisement.field_Reg -> JsString(firstRegistration  match {
       case Some(date) =>  date.year().get() + "/" + date.monthOfYear().get()
       case None => "Unknown"
     })
@@ -31,4 +31,12 @@ case class NewCarAd(id: Long, title: String, fuel: FuelType, price: Long)
   extends Advertisement(id, title, fuel, price, isNew = true, mileage = 0, firstRegistration = None)
 
 
-
+object Advertisement {
+  val field_Id = "id"
+  val field_Title = "title"
+  val field_Fuel = "fuel"
+  val field_Price = "price"
+  val field_New = "new"
+  val field_Mile = "mileage"
+  val field_Reg = "firstRegistration"
+}
